984b5f91fefbe408d6d80e23a8602e590914aac2
ArtemDaemon <artem.daemon.official@gmail.com>
2023-09-13 20:01:18 +0700

    Add function getSecretNumber(int min, int max) to generate random int from min to max

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <random>
+
+int getSecretNumber(int min, int max);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int secretNumber = getSecretNumber(0, 100);
+    std::cout << "New secret number is... " << secretNumber << std::endl;
     return 0;
+}
+
+// Function to generate random int from min to max
+int getSecretNumber(int min, int max) {
+    std::random_device device;
+    std::mt19937 rng(device());
+    std::uniform_int_distribution<int> dist(min, max);
+    return dist(rng);
 }
